c3ffc0baf73f6deae1f5693e82692a5ab710fab6
EomJiChan <akcks101@gmail.com>
2022-09-24 12:08:07 +0900

    update | 20220924

--- homework_js/js14.js
+++ homework_js/js14.js
@@ -1,26 +1,44 @@
 let scores = [100, 90, 100];
 console.log(scores);
 
-console.log(scores[0]);
-console.log(scores[1]);
-console.log(scores[2]);
-console.log(scores[3]);
-console.log(scores.length);
+console.log(scores[0]); // 100
+console.log(scores[1]); // 90
+console.log(scores[2]); // 100
+console.log(scores[3]); // undefined
+console.log(scores.length); // 3
 
 let person = ['John', 'Doe', 26, true];
 for (let idx in person) {
   console.log(person[idx], typeof person[idx]);
 }
+/* result : 
+John string
+Doe string
+26 number
+true boolean
+*/
 
 let student = ['홍길동', [100, 90, 20]];
 for (let idx in student) {
-  console.log(student[idx], typeof student[idx]);
   console.log(idx);
+  console.log(student[idx], typeof student[idx]);
 }
+/*
+0
+홍길동 string
+1
+[100, 90, 20] object
+홍길동
+*/
 for (let element of student) {
   console.log(element);
 }
 console.log(student);
+/*
+홍길동
+[100, 90, 20]
+[ '홍길동', '100, 90, 20']
+*/
 
 let cars = new Array();
 let Cars = new Array('Sasb', 'Volvo', 'Bmw');
@@ -55,6 +73,19 @@ console.log(shape[1]);
 console.log(shape.length);
 console.log(shape);
 
-shape.forEach(function (element, index) {
-  console.log(index, element);
+shape.forEach(function (a, b) {
+  console.log(b, a);
 });
+shape.forEach(function (a) {
+  console.log(a);
+});
+shape.forEach((a, b) => console.log(b, a));
+shape.forEach((a) => console.log(a));
+
+const numbers = [4, 9, 16, 26];
+const res = numbers.map(function (element, index) {
+  return element * 2;
+});
+console.log(typeof numbers, numbers instanceof Object);
+console.log(typeof numbers, numbers instanceof Array);
+console.log(res, typeof res, res instanceof Array);
